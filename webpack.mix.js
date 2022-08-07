189c5b7b9c43b3e83aa6efd878450961df74d13f
Romanch13 <zloy13081997@gmail.com>
2022-08-07 14:16:35 +0200

    Completed project structure
    Description
    1) Clear project
    2) Work function (px to rem)
    3) Sass build
    4) Webpack.config
    5) BrowserSync + correct config
    6) Config package.json
    7) Added Fonts (font-awesome), Bootstrap, Jquery

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,9 +2,12 @@ let mix = require('laravel-mix');
 
 mix.js('src/js/*.js', 'js');
 mix.sass('src/sass/app.sass', 'css');
+
 mix.browserSync({
-  server: {},
-  files: ['public/**/*', '*.html'],
+  server: {
+    baseDir: 'public',
+  },
+  files: ['public/**/*'],
 });
 
 mix.disableNotifications();
